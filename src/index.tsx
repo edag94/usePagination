64119ded84d873767c7742c6de35eab91840ebe0
Erick Dagenais <edag1994@gmail.com>
2021-08-21 20:04:58 -0400

    init hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,62 @@
 import * as React from 'react';
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+export interface PaginationConfig {
+  initialItemCount: number,
+  initialPageNumber: number,
+  initialItemsPerPage: number
+}
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+export const usePagination = ({initialItemCount, initialPageNumber = 1, initialItemsPerPage = 10}: PaginationConfig) => {
+  const [currentPage, setCurrentPage] = React.useState(initialPageNumber);
+  const [itemsPerPage, _setItemsPerPage] = React.useState(initialItemsPerPage);
 
-  return counter;
+  const lastItemIndex = initialItemCount - 1;
+  const maxPage = Math.floor(lastItemIndex / itemsPerPage) + 1; // if remainder, need an extra page
+
+  const canGetNext = currentPage < maxPage;
+  const canGetPrev = currentPage > 1;
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = currentPage !== maxPage ? startIndex + itemsPerPage : initialItemCount;
+
+  const setItemsPerPage = (newItemsPerPage: number) => {
+    const newStartIndexPageNumber = Math.floor(startIndex / newItemsPerPage) + 1;
+    setCurrentPage(newStartIndexPageNumber);
+    _setItemsPerPage(newItemsPerPage);
+  };
+
+  const currentData = (data: unknown[]) => {
+    return data.slice(startIndex, endIndex)
+  }
+
+  const next = () => {
+    if (canGetNext) {
+      setCurrentPage((currentPage) => currentPage + 1);
+    }
+  };
+
+  const prev = () => {
+    if (canGetPrev) {
+      setCurrentPage((currentPage) => currentPage - 1);
+    }
+  };
+
+  const jump = (page: number) => {
+    const pageNumber = Math.max(1, page);
+    setCurrentPage((_currentPage) => Math.min(pageNumber, maxPage));
+  };
+
+  return {
+    startIndex,
+    endIndex,
+    canGetNext,
+    next,
+    canGetPrev,
+    prev,
+    jump,
+    currentData,
+    currentPage,
+    maxPage,
+    setItemsPerPage,
+  };
 };
